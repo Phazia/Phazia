5503b25a276fa2b4a32fa2f87bde6c27477285b3
Charan Sriram <phazia.co@gmail.com>
2020-05-01 15:49:41 -0500

    Initialized project

---
+++ Phazia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Phazia
+//
+//  Created by Charan Sriram on 5/1/20.
+//  Copyright © 2020 Charan Sriram. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhaziaTests/PhaziaTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhaziaTests.swift
+//  PhaziaTests
+//
+//  Created by Charan Sriram on 5/1/20.
+//  Copyright © 2020 Charan Sriram. All rights reserved.
+//
+
+import XCTest
+@testable import Phazia
+
+class PhaziaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
